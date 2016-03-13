443ed3ee7b2fc36a23fc4c90bc1cbce58f61dfeb
Kara de la Marck <karadelamarck@gmail.com>
2016-03-13 13:43:47 +0000

    new page= review new page= questions

--- app/src/main/java/com/studybuddy/studybuddy/homepage.java
+++ app/src/main/java/com/studybuddy/studybuddy/homepage.java
@@ -2,6 +2,9 @@ package com.studybuddy.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
 
 public class homepage extends AppCompatActivity {
 
@@ -9,5 +12,9 @@ public class homepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+    }
+    public void GoToShowRevisionSchedule (View view) {
+        Intent intent = new Intent(this, ShowRevisionSchedule.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/studybuddy/studybuddy/questionaire.java
@@ -0,0 +1,16 @@
+package com.studybuddy.studybuddy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class questionaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_questionaire);
+    }
+
+
+
+}
