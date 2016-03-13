17967b01c5038f4ff44244342fd3ce2f065e3b15
Kara de la Marck <karadelamarck@gmail.com>
2016-03-13 11:51:01 +0000

    logo to login/ sign up page button new page = usernamepage

--- app/src/main/java/com/studybuddy/studybuddy/usernamepage.java
+++ app/src/main/java/com/studybuddy/studybuddy/usernamepage.java
@@ -2,6 +2,10 @@ package com.studybuddy.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+
+
 
 public class usernamepage extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class usernamepage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usernamepage);
+    }
+    public void Gotohomepage (View view) {
+        Intent intent = new Intent(this, homepage.class);
+        startActivity(intent);
     }
 }
